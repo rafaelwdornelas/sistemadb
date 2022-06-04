47cf2f3160e42311f73be59960dd3660dbd55e65
rafael dornelas <rafaelwdornelas@gmail.com>
2022-06-04 07:43:03 -0300

    update cliente

--- api/models/clientes.js
+++ api/models/clientes.js
@@ -88,7 +88,11 @@ module.exports = (sequelize, DataTypes) => {
             uppercase: true,
             validate: {
                 funcaoValidadora: function(dado) {
-                    if (dado.length < 3) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_NOME deve ser um string e não " + typeof dado
+                        );
+                    } else if (dado.length < 3 && dado.length > 0) {
                         throw new Error("O CLI_NOME deve ter no mínimo 3 caracteres");
                     } else if (dado.length > 60) {
                         throw new Error("O CLI_NOME deve ter no máximo 60 caracteres");
@@ -101,7 +105,12 @@ module.exports = (sequelize, DataTypes) => {
             uppercase: true,
             validate: {
                 funcaoValidadora: function(dado) {
-                    if (dado.length < 3) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_NOME_FANTASIA deve ser um string e não " +
+                            typeof dado
+                        );
+                    } else if (dado.length < 3 && dado.length > 0) {
                         throw new Error(
                             "O nome fantasia deve ter no mínimo 3 caracteres"
                         );
@@ -118,8 +127,12 @@ module.exports = (sequelize, DataTypes) => {
             uppercase: true,
             validate: {
                 funcaoValidadora: function(dado) {
-                    if (dado != "M" && dado != "F") {
-                        throw new Error("O sexo deve ser M ou F");
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_SEXO deve ser um string e não " + typeof dado
+                        );
+                    } else if (dado != "M" && dado != "F" && dado != "") {
+                        throw new Error("O sexo deve ser M ou F ou vazio");
                     }
                 },
             },
@@ -129,8 +142,12 @@ module.exports = (sequelize, DataTypes) => {
             uppercase: true,
             validate: {
                 funcaoValidadora: function(dado) {
-                    if (dado != "F" && dado != "J") {
-                        throw new Error("O tipo deve ser F ou J");
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_TIPO deve ser um string e não " + typeof dado
+                        );
+                    } else if (dado != "F" && dado != "J" && dado != "") {
+                        throw new Error("O tipo deve ser F ou J ou vazio");
                     }
                 },
             },
@@ -140,7 +157,11 @@ module.exports = (sequelize, DataTypes) => {
             uppercase: true,
             validate: {
                 funcaoValidadora: function(dado) {
-                    if (dado.length < 11) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_CPF_CNPJ deve ser um string e não " + typeof dado
+                        );
+                    } else if (dado.length < 11 && dado.length > 0) {
                         throw new Error("O CPF/CNPJ deve ter no mínimo 11 caracteres");
                     } else if (dado.length > 14) {
                         throw new Error(
@@ -150,13 +171,31 @@ module.exports = (sequelize, DataTypes) => {
                 },
             },
         }, //CPF ou CNPJ
-        CLI_RG: { type: DataTypes.STRING(15), uppercase: true }, // RG
+        CLI_RG: {
+            type: DataTypes.STRING(15),
+            uppercase: true,
+            validate: {
+                funcaoValidadora: function(dado) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_RG deve ser uma string e não " + typeof dado
+                        );
+                    } else if (dado.length < 5 && dado.length > 0) {
+                        throw new Error("O CLI_RG deve ter no mínimo 5 caracteres");
+                    }
+                },
+            },
+        }, // RG
         CLI_DTNASC: {
             type: DataTypes.STRING(8),
             uppercase: true,
             validate: {
                 funcaoValidadora: function(dado) {
-                    if (dado.length < 8) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_RG deve ser uma string e não " + typeof dado
+                        );
+                    } else if (dado.length < 8 && dado.length > 0) {
                         throw new Error(
                             "A data de nascimento deve ter no mínimo 8 caracteres"
                         );
@@ -164,8 +203,37 @@ module.exports = (sequelize, DataTypes) => {
                 },
             },
         }, // Data de Nascimento
-        CLI_ESTRANGEIRO: DataTypes.BOOLEAN, //  Flag para avisar que ele é ou não um estrangeiro
-        CLI_PASSAPORTE: { type: DataTypes.STRING(20), uppercase: true }, //  Passaporte do cliente, caso o mesmo seja estrangeiro
+        CLI_ESTRANGEIRO: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                funcaoValidadora: function(dado) {
+                    if (typeof dado != "boolean") {
+                        throw new Error(
+                            "O campo CLI_ESTRANGEIRO deve ser uma boolean e não " +
+                            typeof dado
+                        );
+                    }
+                },
+            },
+        }, //  Flag para avisar que ele é ou não um estrangeiro
+        CLI_PASSAPORTE: {
+            type: DataTypes.STRING(20),
+            uppercase: true,
+            validate: {
+                funcaoValidadora: function(dado) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_PASSAPORTE deve ser uma string e não " +
+                            typeof dado
+                        );
+                    } else if (dado.length < 5 && dado.length > 0) {
+                        throw new Error(
+                            "A data de CLI_PASSAPORTE deve ter no mínimo 5 caracteres"
+                        );
+                    }
+                },
+            },
+        }, //  Passaporte do cliente, caso o mesmo seja estrangeiro
         CLI_EMAIL: {
             type: DataTypes.STRING(60),
             lowercase: true,
@@ -173,6 +241,13 @@ module.exports = (sequelize, DataTypes) => {
                 isEmail: {
                     args: true,
                     msg: "Email inválido",
+                },
+                funcaoValidadora: function(dado) {
+                    if (dado.length < 5 && dado.length > 0) {
+                        throw new Error(
+                            "A data de CLI_EMAIL deve ter no mínimo 10 caracteres"
+                        );
+                    }
                 },
             },
         }, //Email do cliente
@@ -184,25 +259,94 @@ module.exports = (sequelize, DataTypes) => {
                     args: true,
                     msg: "Email inválido",
                 },
+                funcaoValidadora: function(dado) {
+                    if (dado.length < 5 && dado.length > 0) {
+                        throw new Error(
+                            "A data de CLI_EMAIL_COB deve ter no mínimo 10 caracteres"
+                        );
+                    }
+                },
             },
         }, //Email do cliente para cobrança e envios de faturas
-        CLI_SITE: { type: DataTypes.STRING(60), uppercase: true }, //Url do site
+        CLI_SITE: {
+            type: DataTypes.STRING(60),
+            uppercase: true,
+            validate: {
+                funcaoValidadora: function(dado) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_SITE deve ser uma string e não " + typeof dado
+                        );
+                    } else if (dado.length < 5 && dado.length > 0) {
+                        throw new Error(
+                            "A data de CLI_SITE deve ter no mínimo 5 caracteres"
+                        );
+                    }
+                },
+            },
+        }, //Url do site
         CLI_IE: {
             type: DataTypes.STRING(18),
             uppercase: true,
             validate: {
                 funcaoValidadora: function(dado) {
-                    if (dado.length < 14) {
+                    if (typeof dado != "string") {
                         throw new Error(
-                            "A Incrição Estadual deve ter no mínimo 14 caracteres"
+                            "O campo CLI_IE deve ser uma string e não " + typeof dado
+                        );
+                    } else if (dado.length < 14 && dado.length > 0) {
+                        throw new Error(
+                            "O campo CLI_IE deve ter no mínimo 14 caracteres"
                         );
                     }
                 },
             },
         }, // Inscrição Estadual
-        CLI_IM: { type: DataTypes.STRING(18), uppercase: true }, // Inscrição Municipal
-        CLI_IR: { type: DataTypes.STRING(18), uppercase: true }, // Inscrição Rural
-        CLI_CNAE: { type: DataTypes.STRING(7), uppercase: true }, // CNAE
+        CLI_IM: {
+            type: DataTypes.STRING(18),
+            uppercase: true,
+            validate: {
+                funcaoValidadora: function(dado) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_IM deve ser uma string e não " + typeof dado
+                        );
+                    } else if (dado.length < 5 && dado.length > 0) {
+                        throw new Error("O campo CLI_IM deve ter no mínimo 5 caracteres");
+                    }
+                },
+            },
+        }, // Inscrição Municipal
+        CLI_IR: {
+            type: DataTypes.STRING(18),
+            uppercase: true,
+            validate: {
+                funcaoValidadora: function(dado) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_IR deve ser uma string e não " + typeof dado
+                        );
+                    } else if (dado.length < 5 && dado.length > 0) {
+                        throw new Error("O campo CLI_IR deve ter no mínimo 5 caracteres");
+                    }
+                },
+            },
+        }, // Inscrição Rural
+        CLI_CNAE: {
+            type: DataTypes.STRING(7),
+            uppercase: true,
+            validate: {
+                funcaoValidadora: function(dado) {
+                    if (typeof dado != "string") {
+                        throw new Error(
+                            "O campo CLI_IR deve ser uma string e não " + typeof dado
+                        );
+                    } else if (dado.length != 7 && dado.length != 0) {
+                        throw new Error("O campo CLI_CNAE deve ter 7 caracteres");
+                    }
+                },
+            },
+        }, // CNAE
         CLI_NATUREZA: DataTypes.INTEGER, // Natureza da Operação
         CLI_VENDEDOR: DataTypes.INTEGER, // Código do Vendedor Padrão
         CLI_COMISSAO: DataTypes.FLOAT, // Comissão do Vendedor
