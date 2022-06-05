ae0dfa48fa699c4209b3e2574a299b6c17c782fa
rafael dornelas <rafaelwdornelas@gmail.com>
2022-06-05 13:26:27 -0300

    add logs TelefonesController

--- api/controllers/TelefonesController.js
+++ api/controllers/TelefonesController.js
@@ -1,6 +1,8 @@
 /* eslint-disable indent */
 const { TelefonesServices } = require("../services");
 const telefonesServices = new TelefonesServices();
+const { globais } = require("../modules");
+const moduloglobais = new globais();
 
 class TelefonesController {
     static async pegaApagados(req, res) {
@@ -11,31 +13,45 @@ class TelefonesController {
                 count: dados == null ? 0 : dados.length,
                 rows: dados,
             };
+            moduloglobais.log("API: telefonesServices.listaRegistrosApagado", "info");
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: telefonesServices.listaRegistrosApagado ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async pegaApagado(req, res) {
-        const { id } = req.params;
         try {
+            const { id } = req.params;
             const dados = await telefonesServices.consultaRegistroApagado(id);
             let retorno = {
                 sucesso: true,
                 count: dados == null ? 0 : 1,
                 row: dados,
             };
+            moduloglobais.log(
+                "API: telefonesServices.consultaRegistroApagado, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: telefonesServices.consultaRegistroApagado ERROR: " +
+                error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
@@ -53,50 +69,72 @@ class TelefonesController {
                     limit: req.body.limit,
                 })),
             };
+            moduloglobais.log(
+                "API: telefonesServices.pegaTodosOsRegistrosWherePaginacao, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: telefonesServices.pegaTodosOsRegistrosWherePaginacao ERROR: " +
+                error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async pega(req, res) {
-        const { id } = req.params;
         try {
+            const { id } = req.params;
             const dados = await telefonesServices.pegaUmRegistro({ id });
             let retorno = {
                 sucesso: true,
                 count: dados == null ? 0 : 1,
                 row: dados,
             };
+            moduloglobais.log(
+                "API: telefonesServices.pegaUmRegistro, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: telefonesServices.pegaUmRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async cria(req, res) {
-        const novoRegistro = req.body;
         try {
+            const novoRegistro = req.body;
             const dados = await telefonesServices.criaRegistro(novoRegistro);
             let retorno = {
                 sucesso: true,
                 count: 1,
                 row: dados,
             };
+            moduloglobais.log("API: telefonesServices.criaRegistro", "info");
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: telefonesServices.criaRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
@@ -110,30 +148,46 @@ class TelefonesController {
                 sucesso: true,
                 msg: `id ${id} atualizado`,
             };
+            moduloglobais.log(
+                "API: telefonesServices.atualizaRegistro, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: telefonesServices.atualizaRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async apaga(req, res) {
-        const { id } = req.params;
         try {
+            const { id } = req.params;
             await telefonesServices.apagaRegistro(Number(id));
             let retorno = {
                 sucesso: true,
                 msg: `id ${id} deletado`,
             };
+            moduloglobais.log(
+                "API: telefonesServices.apagaRegistro, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: telefonesServices.apagaRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
@@ -146,12 +200,20 @@ class TelefonesController {
                 sucesso: true,
                 msg: `id ${id} restaurado`,
             };
+            moduloglobais.log(
+                "API: telefonesServices.restauraRegistro, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: telefonesServices.restauraRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
