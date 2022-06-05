a95ab6469fe79bee9da295ed7d6beaa2baf822c3
rafael dornelas <rafaelwdornelas@gmail.com>
2022-06-05 12:21:21 -0300

    add logs EnderecosController

--- api/controllers/EnderecosController.js
+++ api/controllers/EnderecosController.js
@@ -1,6 +1,8 @@
 /* eslint-disable indent */
 const { EnderecosServices } = require("../services");
 const enderecosServices = new EnderecosServices();
+const { globais } = require("../modules");
+const moduloglobais = new globais();
 
 class EnderecosController {
     static async pegaApagados(req, res) {
@@ -11,31 +13,45 @@ class EnderecosController {
                 count: dados == null ? 0 : dados.length,
                 rows: dados,
             };
+            moduloglobais.log("API: enderecosServices.listaRegistrosApagado", "info");
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: enderecosServices.listaRegistrosApagado ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async pegaApagado(req, res) {
-        const { id } = req.params;
         try {
+            const { id } = req.params;
             const dados = await enderecosServices.consultaRegistroApagado(id);
             let retorno = {
                 sucesso: true,
                 count: dados == null ? 0 : 1,
                 row: dados,
             };
+            moduloglobais.log(
+                "API: enderecosServices.consultaRegistroApagado, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: enderecosServices.consultaRegistroApagado ERROR: " +
+                error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
@@ -53,105 +69,151 @@ class EnderecosController {
                     limit: req.body.limit,
                 })),
             };
+            moduloglobais.log(
+                "API: enderecosServices.pegaTodosOsRegistrosWherePaginacao, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: enderecosServices.pegaTodosOsRegistrosWherePaginacao ERROR: " +
+                error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async pega(req, res) {
-        const { id } = req.params;
         try {
+            const { id } = req.params;
             const dados = await enderecosServices.pegaUmRegistro({ id });
             let retorno = {
                 sucesso: true,
                 count: dados == null ? 0 : 1,
                 row: dados,
             };
+            moduloglobais.log(
+                "API: enderecosServices.pegaUmRegistro, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: enderecosServices.pegaUmRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async cria(req, res) {
-        const novoRegistro = req.body;
         try {
+            const novoRegistro = req.body;
             const dados = await enderecosServices.criaRegistro(novoRegistro);
             let retorno = {
                 sucesso: true,
                 count: 1,
                 row: dados,
             };
+            moduloglobais.log("API: enderecosServices.criaRegistro", "info");
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: enderecosServices.criaRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async atualiza(req, res) {
-        const { id } = req.params;
-        const novasInfos = req.body;
         try {
+            const { id } = req.params;
+            const novasInfos = req.body;
             await enderecosServices.atualizaRegistro(novasInfos, Number(id));
             let retorno = {
                 sucesso: true,
                 msg: `id ${id} atualizado`,
             };
+            moduloglobais.log(
+                "API: enderecosServices.atualizaRegistro, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: enderecosServices.atualizaRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async apaga(req, res) {
-        const { id } = req.params;
         try {
+            const { id } = req.params;
             await enderecosServices.apagaRegistro(Number(id));
             let retorno = {
                 sucesso: true,
                 msg: `id ${id} deletado`,
             };
+            moduloglobais.log(
+                "API: enderecosServices.apagaRegistro, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: enderecosServices.apagaRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
 
     static async restaura(req, res) {
-        const { id } = req.params;
         try {
+            const { id } = req.params;
             await enderecosServices.restauraRegistro(Number(id));
             let retorno = {
                 sucesso: true,
                 msg: `id ${id} restaurado`,
             };
+            moduloglobais.log(
+                "API: enderecosServices.restauraRegistro, ID: " + id,
+                "info"
+            );
             return res.status(200).json(retorno);
         } catch (error) {
             let retorno = {
                 sucesso: false,
                 msg: error.message,
             };
+            moduloglobais.log(
+                "API: enderecosServices.restauraRegistro ERROR: " + error.message,
+                "error"
+            );
             return res.status(500).json(retorno);
         }
     }
