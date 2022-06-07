76a6390d8a5b07c0d8a25f009d00c7d36194beb4
rafael dornelas <rafaelwdornelas@gmail.com>
2022-06-07 18:19:37 -0300

    criação do modelo funcionarios

--- api/migrations/20220607170246-create-funcionarios.js
+++ api/migrations/20220607170246-create-funcionarios.js
@@ -9,212 +9,235 @@ module.exports = {
                 type: Sequelize.INTEGER,
             },
             FUN_MSOCIAL: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(100),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Numero matricula eSocial
             FUN_NOME: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(100),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Nome do funcionario
             FUN_NOME_MAE: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(100),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Nome da mae do funcionario
             FUN_NOME_PAI: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(100),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Nome do pai do funcionario
             FUN_SEXO: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(1),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Sexo do funcionario M ou F
             FUN_NASCIMENTO: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(8),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Data de nascimento do funcionario YYYYMMDD
             FUN_EST_CIVIL: {
                 type: Sequelize.INTEGER,
                 defaultValue: 1,
                 allowNull: false,
-            },
+            }, //Estado civil do funcionario 1: Solteiro 2: Casado 3: Divorciado 4: Viúvo
             FUN_NACIONALIDADE: {
                 type: Sequelize.INTEGER,
                 defaultValue: 1058,
                 allowNull: false,
-            },
+            }, //Nacionalidade do funcionario
             FUN_EMAIL: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(255),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Email do funcionario
             FUN_DEFICIENTE: {
                 type: Sequelize.BOOLEAN,
                 defaultValue: false,
                 allowNull: false,
-            },
+            }, //Deficiente fisico do funcionario true ou false
             FUN_OBS_DEFICIENCIA: {
-                type: Sequelize.STRING,
+                type: Sequelize.TEXT,
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Observacoes sobre deficiencia do funcionario
             FUN_DATA_ADMISSAO: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(8),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Data de admissao do funcionario YYYYMMDD
             FUN_DATA_DEMISSAO: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(8),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Data de demissao do funcionario YYYYMMDD
             FUN_RECEB_BANCO: {
-                type: Sequelize.INTEGER,
+                type: Sequelize.INTEGER(80),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Recebe banco do funcionario
             FUN_RECEB_AG: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(10),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Recebe agencia do funcionario
             FUN_RECEB_CONTA: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(10),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Recebe conta do funcionario
             FUN_GRUPO: {
                 type: Sequelize.INTEGER,
                 defaultValue: 1,
                 allowNull: false,
-            },
+                references: {
+                    model: "FUNCIONARIOGRUPOS",
+                    key: "id",
+                },
+            }, //Grupo do funcionario
             FUN_CARGO: {
                 type: Sequelize.INTEGER,
                 defaultValue: 1,
                 allowNull: false,
-            },
+                references: {
+                    model: "FUNCIONARIOCARGOS",
+                    key: "id",
+                },
+            }, //Cargo do funcionario
             FUN_SALARIO: {
                 type: Sequelize.FLOAT,
                 defaultValue: 0,
                 allowNull: false,
-            },
+            }, //Salario do funcionario
             FUN_TIPO_CONTRATO: {
                 type: Sequelize.INTEGER,
                 defaultValue: 1,
                 allowNull: false,
-            },
+            }, //Tipo de contrato do funcionario 1: CLT 2: PJ 3: PJ/CLT
             FUN_DATA_FIM_CONTRATO: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(8),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Data de fim do contrato do funcionario YYYYMMDD
             FUN_P_ADIANTAMENTO: {
                 type: Sequelize.FLOAT,
                 defaultValue: 0,
                 allowNull: false,
-            },
+            }, //Percentual de adiantamento do funcionario
             FUN_CPF: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(11),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //CPF do funcionario
             FUN_PIS: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(11),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //PIS do funcionario
             FUN_RG: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(15),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //RG do funcionario
             FUN_RG_EMISSAO: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(8),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //data de emissão
             FUN_RG_UF: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(2),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //UF do RG
             FUN_RG_OEM: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(3),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //orgão  emissor
             FUN_END_RUA: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(255),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Endereco da rua do funcionario
             FUN_END_NUMERO: {
                 type: Sequelize.INTEGER,
                 defaultValue: 0,
                 allowNull: false,
-            },
+            }, //Numero do endereco do funcionario
             FUN_END_BAIRRO: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(45),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Bairro do funcionario
             FUN_END_COMP: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(45),
                 defaultValue: "",
                 allowNull: false,
-            },
-            FUN_END_CIDADE: {
-                type: Sequelize.STRING,
+            }, //Complemento do funcionario
+            FUN_END_MUNICIPIO: {
+                type: Sequelize.STRING(60),
                 defaultValue: "",
                 allowNull: false,
-            },
-            FUN_END_UF: {
-                type: Sequelize.STRING,
+            }, //Municipio do funcionario
+            FUN_END_EST: {
+                type: Sequelize.STRING(2),
                 defaultValue: "",
                 allowNull: false,
-            },
-            FUN_END_CPF: {
-                type: Sequelize.STRING,
+            }, //Estado do funcionario
+            FUN_END_CEP: {
+                type: Sequelize.STRING(10),
                 defaultValue: "",
                 allowNull: false,
-            },
-            FUN_TELEFONE: {
-                type: Sequelize.STRING,
+            }, //CEP do funcionario
+            FUN_TEL_DDD: {
+                type: Sequelize.STRING(2),
                 defaultValue: "",
                 allowNull: false,
-            },
-            FUN_CELULAR: {
-                type: Sequelize.STRING,
+            }, //DDD do telefone do funcionario
+            FUN_TEL_NUMERO: {
+                type: Sequelize.STRING(9),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //Numero do telefone do funcionario
+            FUN_CELULAR_DDD: {
+                type: Sequelize.STRING(2),
+                defaultValue: "",
+                allowNull: false,
+            }, //DDD do celular do funcionario
+            FUN_CELULAR_NUMERO: {
+                type: Sequelize.STRING(9),
+                defaultValue: "",
+                allowNull: false,
+            }, //Numero do celular do funcionario
             FUN_EMER_CONTATO: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(80),
                 defaultValue: "",
                 allowNull: false,
-            },
-            FUN_EMER_TELEFONE: {
-                type: Sequelize.STRING,
+            }, //nome do contato de emergencia do funcionario
+            FUN_EMER_DDD: {
+                type: Sequelize.STRING(2),
                 defaultValue: "",
                 allowNull: false,
-            },
+            }, //DDD do telefone de emergencia do funcionario
+            FUN_EMER_NUMERO: {
+                type: Sequelize.STRING(9),
+                defaultValue: "",
+                allowNull: false,
+            }, //Numero do telefone de emergencia do funcionario
             CRIADO_EM: {
                 allowNull: false,
                 type: Sequelize.DATE,
-            },
+            }, //Data de criacao do registro
             ATUALIZADO_EM: {
                 allowNull: false,
                 type: Sequelize.DATE,
-            },
+            }, //Data de atualizacao do registro
             DELETADO_EM: {
                 allowNull: true,
                 type: Sequelize.DATE,
-            },
+            }, //Data de exclusao do registro
         });
     },
     async down(queryInterface) {
--- api/models/funcionarios.js
+++ api/models/funcionarios.js
@@ -8,45 +8,142 @@ module.exports = (sequelize, DataTypes) => {
         }
     }
     FUNCIONARIOS.init({
-        FUN_MSOCIAL: DataTypes.STRING,
-        FUN_NOME: DataTypes.STRING,
-        FUN_NOME_MAE: DataTypes.STRING,
-        FUN_NOME_PAI: DataTypes.STRING,
-        FUN_SEXO: DataTypes.STRING,
-        FUN_NASCIMENTO: DataTypes.STRING,
-        FUN_EST_CIVIL: DataTypes.INTEGER,
-        FUN_NACIONALIDADE: DataTypes.INTEGER,
-        FUN_EMAIL: DataTypes.STRING,
-        FUN_DEFICIENTE: DataTypes.BOOLEAN,
-        FUN_OBS_DEFICIENCIA: DataTypes.STRING,
-        FUN_DATA_ADMISSAO: DataTypes.STRING,
-        FUN_DATA_DEMISSAO: DataTypes.STRING,
-        FUN_RECEB_BANCO: DataTypes.INTEGER,
-        FUN_RECEB_AG: DataTypes.STRING,
-        FUN_RECEB_CONTA: DataTypes.STRING,
-        FUN_GRUPO: DataTypes.INTEGER,
-        FUN_CARGO: DataTypes.INTEGER,
-        FUN_SALARIO: DataTypes.FLOAT,
-        FUN_TIPO_CONTRATO: DataTypes.INTEGER,
-        FUN_DATA_FIM_CONTRATO: DataTypes.STRING,
-        FUN_P_ADIANTAMENTO: DataTypes.FLOAT,
-        FUN_CPF: DataTypes.STRING,
-        FUN_PIS: DataTypes.STRING,
-        FUN_RG: DataTypes.STRING,
-        FUN_RG_EMISSAO: DataTypes.STRING,
-        FUN_RG_UF: DataTypes.STRING,
-        FUN_RG_OEM: DataTypes.STRING,
-        FUN_END_RUA: DataTypes.STRING,
-        FUN_END_NUMERO: DataTypes.STRING,
-        FUN_END_BAIRRO: DataTypes.STRING,
-        FUN_END_COMP: DataTypes.STRING,
-        FUN_END_CIDADE: DataTypes.STRING,
-        FUN_END_UF: DataTypes.STRING,
-        FUN_END_CPF: DataTypes.STRING,
-        FUN_TELEFONE: DataTypes.STRING,
-        FUN_CELULAR: DataTypes.STRING,
-        FUN_EMER_CONTATO: DataTypes.STRING,
-        FUN_EMER_TELEFONE: DataTypes.STRING,
+        FUN_MSOCIAL: {
+            type: DataTypes.STRING(100),
+        }, //Numero matricula eSocial
+        FUN_NOME: {
+            type: DataTypes.STRING(100),
+            uppercase: true,
+        }, //Nome do funcionario
+        FUN_NOME_MAE: {
+            type: DataTypes.STRING(100),
+            uppercase: true,
+        }, //Nome da mae do funcionario
+        FUN_NOME_PAI: {
+            type: DataTypes.STRING(100),
+            uppercase: true,
+        }, //Nome do pai do funcionario
+        FUN_SEXO: {
+            type: DataTypes.STRING(1),
+            uppercase: true,
+        }, //Sexo do funcionario M ou F
+        FUN_NASCIMENTO: {
+            type: DataTypes.STRING(8),
+        }, //Data de nascimento do funcionario YYYYMMDD
+        FUN_EST_CIVIL: {
+            type: DataTypes.INTEGER,
+        }, //Estado civil do funcionario 1: Solteiro 2: Casado 3: Divorciado 4: Viúvo
+        FUN_NACIONALIDADE: {
+            type: DataTypes.INTEGER,
+        }, //Nacionalidade do funcionario
+        FUN_EMAIL: {
+            type: DataTypes.STRING(255),
+        }, //Email do funcionario
+        FUN_DEFICIENTE: {
+            type: DataTypes.BOOLEAN,
+        }, //Deficiente fisico do funcionario true ou false
+        FUN_OBS_DEFICIENCIA: {
+            type: DataTypes.TEXT,
+        }, //Observacoes sobre deficiencia do funcionario
+        FUN_DATA_ADMISSAO: {
+            type: DataTypes.STRING(8),
+        }, //Data de admissao do funcionario YYYYMMDD
+        FUN_DATA_DEMISSAO: {
+            type: DataTypes.STRING(8),
+        }, //Data de demissao do funcionario YYYYMMDD
+        FUN_RECEB_BANCO: {
+            type: DataTypes.INTEGER(80),
+        }, //Recebe banco do funcionario
+        FUN_RECEB_AG: {
+            type: DataTypes.STRING(10),
+        }, //Recebe agencia do funcionario
+        FUN_RECEB_CONTA: {
+            type: DataTypes.STRING(10),
+        }, //Recebe conta do funcionario
+        FUN_GRUPO: {
+            type: DataTypes.INTEGER,
+        }, //Grupo do funcionario
+        FUN_CARGO: {
+            type: DataTypes.INTEGER,
+        }, //Cargo do funcionario
+        FUN_SALARIO: {
+            type: DataTypes.FLOAT,
+        }, //Salario do funcionario
+        FUN_TIPO_CONTRATO: {
+            type: DataTypes.INTEGER,
+        }, //Tipo de contrato do funcionario 1: CLT 2: PJ 3: PJ/CLT
+        FUN_DATA_FIM_CONTRATO: {
+            type: DataTypes.STRING(8),
+        }, //Data de fim do contrato do funcionario YYYYMMDD
+        FUN_P_ADIANTAMENTO: {
+            type: DataTypes.FLOAT,
+        }, //Percentual de adiantamento do funcionario
+        FUN_CPF: {
+            type: DataTypes.STRING(11),
+        }, //CPF do funcionario
+        FUN_PIS: {
+            type: DataTypes.STRING(11),
+        }, //PIS do funcionario
+        FUN_RG: {
+            type: DataTypes.STRING(15),
+        }, //RG do funcionario
+        FUN_RG_EMISSAO: {
+            type: DataTypes.STRING(8),
+        }, //data de emissão
+        FUN_RG_UF: {
+            type: DataTypes.STRING(2),
+        }, //UF do RG
+        FUN_RG_OEM: {
+            type: DataTypes.STRING(3),
+        }, //orgão  emissor
+        FUN_END_RUA: {
+            type: DataTypes.STRING(255),
+            uppercase: true,
+        }, //Endereco da rua do funcionario
+        FUN_END_NUMERO: {
+            type: DataTypes.INTEGER,
+        }, //Numero do endereco do funcionario
+        FUN_END_BAIRRO: {
+            type: DataTypes.STRING(45),
+            uppercase: true,
+        }, //Bairro do funcionario
+        FUN_END_COMP: {
+            type: DataTypes.STRING(45),
+            uppercase: true,
+        }, //Complemento do funcionario
+        FUN_END_MUNICIPIO: {
+            type: DataTypes.STRING(60),
+            uppercase: true,
+        }, //Municipio do funcionario
+        FUN_END_EST: {
+            type: DataTypes.STRING(2),
+            uppercase: true,
+        }, //Estado do funcionario
+        FUN_END_CEP: {
+            type: DataTypes.STRING(10),
+        }, //CEP do funcionario
+        FUN_TEL_DDD: {
+            type: DataTypes.STRING(2),
+        }, //DDD do telefone do funcionario
+        FUN_TEL_NUMERO: {
+            type: DataTypes.STRING(9),
+        }, //Numero do telefone do funcionario
+        FUN_CELULAR_DDD: {
+            type: DataTypes.STRING(2),
+        }, //DDD do celular do funcionario
+        FUN_CELULAR_NUMERO: {
+            type: DataTypes.STRING(9),
+        }, //Numero do celular do funcionario
+        FUN_EMER_CONTATO: {
+            type: DataTypes.STRING(80),
+            uppercase: true,
+        }, //nome do contato de emergencia do funcionario
+        FUN_EMER_DDD: {
+            type: DataTypes.STRING(2),
+        }, //DDD do telefone de emergencia do funcionario
+        FUN_EMER_NUMERO: {
+            type: DataTypes.STRING(9),
+        }, //Numero do telefone de emergencia do funcionario
     }, {
         sequelize,
         modelName: "FUNCIONARIOS",
