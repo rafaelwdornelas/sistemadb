c3f3b0977268185f7308dc27ee367482f6429970
rafael dornelas <rafaelwdornelas@gmail.com>
2022-06-08 11:52:29 -0300

    add model prodfamilias

---
+++ api/models/prodfamilias.js
@@ -0,0 +1,22 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class PRODFAMILIAS extends Model {
+        // eslint-disable-next-line no-unused-vars
+        static associate(models) {
+            // define association here
+        }
+    }
+    PRODFAMILIAS.init({
+        PROD_NOME: DataTypes.STRING,
+    }, {
+        sequelize,
+        modelName: "PRODFAMILIAS",
+        createdAt: "CRIADO_EM",
+        updatedAt: "ATUALIZADO_EM",
+        deletedAt: "DELETADO_EM",
+        freezeTableName: true,
+        paranoid: true,
+    });
+    return PRODFAMILIAS;
+};
