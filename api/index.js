fb4e042c60563da8a87ddf0cc080793e8317fba6
rafael dornelas <rafaelwdornelas@gmail.com>
2022-06-20 19:56:23 -0300

    update rapidoc

--- api/index.js
+++ api/index.js
@@ -3,11 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
 const helmet = require("helmet");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-const fs = require("fs");
 const { globais } = require("./modules");
 const moduloglobais = new globais();
 
@@ -23,7 +22,7 @@ const swaggerDefinition = {
       email: "contato@corebyteerp.com.br",
     },
   },
-  /*  servers: [
+  servers: [
     {
       url: "http://127.0.0.1:37778",
       description: "Localhost",
@@ -32,7 +31,7 @@ const swaggerDefinition = {
       url: "http://207.244.230.240:37778",
       description: "CoreByte ERP",
     },
-  ], */
+  ],
   termsOfService: "http://swagger.io/terms/",
   schemes: ["http", "https"],
   consumes: ["application/json"],
@@ -54,38 +53,19 @@ const swaggerDefinition = {
   ],
 };
 
-fs.readFile("layout.css", "utf8", function (err, data) {
-  if (err) throw err;
-  var cssOptions = {
-    customCss: data,
-    customSiteTitle: "CoreByte ERP - API",
-    explorer: false,
-  };
+const options = {
+  swaggerDefinition,
+  apis: ["./api/*.js", "./api/routes/*.js", "./components.yaml"],
+};
 
-  const options = {
-    swaggerDefinition,
-    apis: ["./api/*.js", "./api/routes/*.js", "./components.yaml"],
-  };
+const swaggerDocs = swaggerJsDoc(options);
 
-  const swaggerDocs = swaggerJsDoc(options);
+app.get("/api-docs.json", (req, res) => {
+  res.send(swaggerDocs);
+});
 
-  app.get("/api-docs.json", (req, res) => {
-    res.send(swaggerDocs);
-  });
-
-  if (process.env.SWAGGERUICSS == "1") {
-    app.use(
-      "/",
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerDocs, cssOptions, { docExpansion: "none" })
-    );
-  } else {
-    app.use(
-      "/",
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerDocs, false, { docExpansion: "none" })
-    );
-  }
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/documentation.html"));
 });
 
 const allowCrossDomain = async (req, res, next) => {
