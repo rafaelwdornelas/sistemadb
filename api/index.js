fca37dd58d8455b76e979a5130ab33311539c1b6
rafael dornelas <rafaelwdornelas@gmail.com>
2022-06-28 02:57:29 -0300

    add cluster

--- api/index.js
+++ api/index.js
@@ -5,11 +5,12 @@ const routes = require("./routes");
 const swaggerJsDoc = require("swagger-jsdoc");
 const helmet = require("helmet");
 const path = require("path");
-const app = express();
 const fs = require("fs");
 const port = process.env.PORT || 3000;
 const { globais } = require("./modules");
 const moduloglobais = new globais();
+const cluster = require("cluster");
+const totalCPUs = require("os").cpus().length;
 
 const swaggerDefinition = {
   openapi: "3.0.0",
@@ -46,59 +47,77 @@ const swaggerDefinition = {
   ],
 };
 
-app.get("/api-docs.json", (req, res) => {
-  swaggerDefinition.servers = [
-    {
-      url: `http://${req.hostname}:${port}`,
-      description: "CoreByte ERP",
-    },
-  ];
-  const options = {
-    swaggerDefinition,
-    apis: ["./api/*.js", "./api/routes/*.js", "./components.yaml"],
+if (cluster.isMaster) {
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  const app = express();
+  app.get("/api-docs.json", (req, res) => {
+    swaggerDefinition.servers = [
+      {
+        url: `http://${req.hostname}:${port}`,
+        description: "CoreByte ERP",
+      },
+    ];
+    const options = {
+      swaggerDefinition,
+      apis: ["./api/*.js", "./api/routes/*.js", "./components.yaml"],
+    };
+
+    const swaggerDocs = swaggerJsDoc(options);
+    var paths = sortObjectByKeys(swaggerDocs.paths);
+    swaggerDocs.paths = paths;
+    res.send(swaggerDocs);
+  });
+
+  app.get("/api-docs", function (req, res) {
+    let html = fs.readFileSync(
+      path.join(__dirname + "/documentation.html"),
+      "utf8"
+    );
+    html = html.replace("{{server}}", `http://${req.hostname}:${port}`);
+    res.send(html);
+  });
+
+  const allowCrossDomain = async (req, res, next) => {
+    let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PUT, POST, PATCH, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Xss-Protection", true);
+    res.header("Sql-Injection-Protection", true);
+    res.header("Connection-From", ip);
+    next();
   };
 
-  const swaggerDocs = swaggerJsDoc(options);
-  var paths = sortObjectByKeys(swaggerDocs.paths);
-  swaggerDocs.paths = paths;
-  res.send(swaggerDocs);
-});
-
-app.get("/api-docs", function (req, res) {
-  let html = fs.readFileSync(
-    path.join(__dirname + "/documentation.html"),
-    "utf8"
+  app.use(allowCrossDomain);
+  app.use(helmet());
+  app.use(cors());
+  app.use(express.static("public"));
+  routes(app);
+  app.listen(port, () =>
+    moduloglobais.log(
+      `Servidor iniciado na porta: ${port} no Cluster ${process.pid}`,
+      "success"
+    )
   );
-  html = html.replace("{{server}}", `http://${req.hostname}:${port}`);
-  res.send(html);
-});
-
-const allowCrossDomain = async (req, res, next) => {
-  let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, PATCH, DELETE, OPTIONS"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Xss-Protection", true);
-  res.header("Sql-Injection-Protection", true);
-  res.header("Connection-From", ip);
-  next();
-};
-
-app.use(allowCrossDomain);
-app.use(helmet());
-app.use(cors());
-app.use(express.static("public"));
-routes(app);
-moduloglobais.log("Iniciando API...", "");
-app.listen(port, () =>
-  moduloglobais.log(`Servidor iniciado na porta: ${port}`, "success")
-);
-
-module.exports = app;
+  module.exports = app;
+}
 
 function sortObjectByKeys(o) {
   return Object.keys(o)
